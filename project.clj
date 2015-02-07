2a203049f3a567d50502ed023bde38173f8442c8
Tom Crayford <tcrayford@googlemail.com>
2015-02-07 23:00:02 +0000

    change github link

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject yeller/fressian-clojure "0.3.0"
   :description "Clojure wrapper for the Fressian library"
-  :url "http://github.com/vitalreactor/fressian-clojure"
+  :url "http://github.com/yeller/fressian-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :global-vars {*warn-on-reflection* true}
